a00c46325b3b1ddac0af5ca69ee36a10629d7e22
sephirot47 <victorantondominguez@gmail.com>
2018-03-30 21:08:05 +0200

    Added path exporting to buildPackage

--- Scripts/BuildPackage/buildPackage.sh
+++ Scripts/BuildPackage/buildPackage.sh
@@ -4,5 +4,6 @@ cd "$(dirname "$0")"
 
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j6
+NEWPATH="$PATH:/usr/lib/x86_64-linux-gnu/"
+PATH=$NEWPATH cmake -DCMAKE_BUILD_TYPE=Release ..
+PATH=$NEWPATH make -j6

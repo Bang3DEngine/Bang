1223d4074d1ad0027552f21a9565d586a9da76dd
sephirot47 <victorantondominguez@gmail.com>
2018-04-01 19:17:42 +0200

    Trying to fix coverity on Travis now...

--- Scripts/coverity.sh
+++ Scripts/coverity.sh
@@ -8,5 +8,5 @@ echo -e "#!/bin/bash \n\n mkdir -p build \n cd build \n cmake .. \n make -j6" > 
 chmod +x ${TMP_COMPILE_FILE}
 cov-build --dir cov-int ./${TMP_COMPILE_FILE}
 rm ${TMP_COMPILE_FILE}
-rm -rf cov-int
 tar -czvf Bang.tgz cov-int
+rm -rf cov-int
